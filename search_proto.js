a802b15ac0adfbf4dab1d9a18889fe9c5ee68cb1
asngai <andrewngai.ucsb@gmail.com>
2015-11-10 11:37:17 -0800

    search engine prototype for reference

---
+++ search_proto.js
@@ -0,0 +1,18 @@
+
+
+apis.twitter.getData('Drake',function(success,response){
+    var success = success;
+    console.log(success);
+    var response = response;
+    console.log(response);
+    var if_retweeted = response.tweets.status[0].retweet;
+    console.log(if_retweeted);//logs a bool for first tweet in response obj
+    var retweet_count = response.tweets.status[0].retweet_count;
+    console.log(retweet_count);//logs the retweet count for first tweet in response obj
+    var body_text = response.tweets.status[0].text;
+    console.log(body_text);//logs the main text for first tweet in response obj
+    var username = response.tweets.status[0].user.name;
+    console.log(username);//logs the username for first tweet in response obj
+});
+
+
