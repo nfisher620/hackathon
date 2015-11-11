34cd0b497ef1ef10b0164fa536e28e687b3da0b2
asngai <andrewngai.ucsb@gmail.com>
2015-11-11 10:17:14 -0800

    autosave

--- Tweet.js
+++ Tweet.js
@@ -1,6 +1,15 @@
 /**
  * Created by nicolespence on 11/10/15.
  */
+/**
+ * Creates an instance of Tweet.
+ *
+ * @constructor
+ * @this {Tweet}
+ * @param {retweet_count} the number of associated retweets.
+ * @param {body_text} the main text of tweet obj.
+ * @param {username} the username of the tweeter.
+ */
 function Tweet(retweet_count, body_text, username) {
     this;
     this.retweet_count = retweet_count;
